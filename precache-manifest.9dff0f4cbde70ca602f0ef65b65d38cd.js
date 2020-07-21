8440c1ff8abd05532f78f7f127486b8a9cf31500
Marc Palmer <mapalmer@hbi.com>
2020-07-21 14:56:00 -0500

    Updates

---
+++ precache-manifest.9dff0f4cbde70ca602f0ef65b65d38cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79be92f8be7c4140e990687eba5bff3a",
+    "url": "/ironfist/index.html"
+  },
+  {
+    "revision": "1f70713d1943d7fcfa42",
+    "url": "/ironfist/static/css/main.b86b8bd9.chunk.css"
+  },
+  {
+    "revision": "e0ab97999bff46655d1f",
+    "url": "/ironfist/static/js/2.78ad707e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ironfist/static/js/2.78ad707e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f70713d1943d7fcfa42",
+    "url": "/ironfist/static/js/main.d3f795ad.chunk.js"
+  },
+  {
+    "revision": "2096842c7e743d8931ea",
+    "url": "/ironfist/static/js/runtime-main.86349990.js"
+  },
+  {
+    "revision": "aa5db8dc0192b70ed3217d095fb5ab8c",
+    "url": "/ironfist/static/media/logo.aa5db8dc.svg"
+  }
+]);
