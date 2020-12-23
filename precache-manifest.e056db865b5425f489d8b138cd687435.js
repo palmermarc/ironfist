bd0669703ecd0696267d86e15e0fc8af984743ba
Marc Palmer <mapalmer@hbi.com>
2020-12-23 08:30:13 -0600

    Updates

---
+++ precache-manifest.e056db865b5425f489d8b138cd687435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923b0d219e38df3b12ac45054246eaca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe6b100316b0d7ba8bc1",
+    "url": "/static/css/main.fd085dc4.chunk.css"
+  },
+  {
+    "revision": "217aee0d5b63e0449ecd",
+    "url": "/static/js/2.a1407110.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.a1407110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6b100316b0d7ba8bc1",
+    "url": "/static/js/main.a3d0a3ab.chunk.js"
+  },
+  {
+    "revision": "08c5b4c75928dc785400",
+    "url": "/static/js/runtime-main.bc79ebd3.js"
+  },
+  {
+    "revision": "625c6fb36cff2bfd6abaf98dda87cd7f",
+    "url": "/static/media/logo.625c6fb3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ironfist/precache-manifest.6f7f6afd9fb7dbb58684129391c63050.js"
+  "/precache-manifest.e056db865b5425f489d8b138cd687435.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ironfist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
